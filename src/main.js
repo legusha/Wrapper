a0e272c225983f1946e7fac26c21a73b1e8834c4
vlad.legusha <vlad.legusha@gmail.com>
2021-11-04 12:06:40 +0200

    implemented wrapper and update README.md

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('\n')
+import { Wrapper } from './wrapper'
+
+// Base example how to use wrapper
+const caller = {
+  call: (n) => console.log('CALL\n', n),
+  end: () => console.log('END')
+}
+const callerWrapper = (fnName, fn, ...args) => {
+  console.log(`Function call with name "${fnName}"`)
+  fn(...args)
+}
+const newCaller = new Wrapper(caller, callerWrapper)
+
+newCaller.call('+399 999 999')
+newCaller.end()
