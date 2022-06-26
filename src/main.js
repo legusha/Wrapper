db1b03352b46259f8ec0078617edd3f67714fcef
vlad.legusha <vlad.legusha@gmail.com>
2022-06-26 14:07:18 +0300

    decomposition cache module

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import { Wrapper } from './wrapper'
-import { provider as cashProvider } from './cash.mjs'
+import Wrapper from './wrapper'
+import { cacheProvider } from './cache.mjs'
 
 // Base example how to use wrapper
 const caller = {
   number: 99,
-  call: (n) => console.log('CALL\n', n),
+  call: (n) => console.log('CALL\t', n),
   end: () => console.log('END')
 }
-
 
 const wrapperCaller = (fnName, fn) => {
   return (...args) => {
@@ -15,41 +14,44 @@ const wrapperCaller = (fnName, fn) => {
     fn(...args)
   }
 }
-const wrapperCash = (fnName, fn) => {
-  return (...args) => {
-    return cashProvider(fnName, fn, ...args)
-  }
-}
 const wrapperNumber = (rootVal, val) => {
   console.log(`Function call with root value "${rootVal}" and actual value "${val}"`)
 }
 
 
-
 const callerNew = new Wrapper(caller, wrapperCaller)
-const callerCash = new Wrapper(caller, wrapperCash)
-const callOnce = new Wrapper(caller.call, wrapperCash)
 const phoneNumber = new Wrapper(caller.number, wrapperNumber)
 
 callerNew.call('+399 999 999')
 callerNew.end()
 
+// Int
+phoneNumber.value = 777
+
+
+// Asynchronous cache
+const phoneBook = {
+  liza: '+399 999 999',
+  alice: '+311 111 111'
+}
+const phoneBookFind = (name) => phoneBook[name];
+
+const wrapperCache = (fnName, fn) => {
+  return async (...args) => {
+    return await cacheProvider(fnName, fn, ...args);
+  };
+};
+
+const callerCache = new Wrapper(phoneBookFind, wrapperCache);
 
 // In Functions that were called with the same arguments, the result will be taken from the cache.
-callerCash.call('+399 999 999')
-callerCash.call('+399 999 999')
-callerCash.call('+399 999 333')
-callerCash.call('+399 999 666')
-callerNew.end()
-
-
-callOnce('+399 777 777')
-
-
-// Number
-console.log(phoneNumber.value)
-phoneNumber.value = 777
-console.log(phoneNumber.value)
+(async () => {
+  await callerCache('liza')
+  await callerCache('alice')
+  await callerCache('liza')
+  await callerCache('liza')
+  await callerCache('liza')
+})()
 
 
 
